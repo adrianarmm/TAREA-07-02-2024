2d85f6e027763ceebcb7b70544146c1435170ad8
adrianarmm <areyemor@myuax.com>
2024-02-07 20:02:42 +0100

    97

--- H.java
+++ H.java
@@ -1,2 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class H {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el tamaño de la matriz cuadrada: ");
+        int n = scanner.nextInt();
