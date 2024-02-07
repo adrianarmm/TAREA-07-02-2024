d4d738a67c725270d4ad7ca46a5fcaf15337139c
adrianarmm <areyemor@myuax.com>
2024-02-07 19:31:10 +0100

    64

--- e.java
+++ e.java
@@ -1,2 +1,6 @@
-public class e {
-}
+import java.util.ArrayList;
+
+public class FactoresPrimos {
+    public static void main(String[] args) {
+        int numero = 40;
+        int[] factoresPrimos = descomponerEnFactoresPrimos(numero);
