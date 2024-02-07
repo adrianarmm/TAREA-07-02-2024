69339a14422a1c56c497cb061d4cbd4033616722
adrianarmm <areyemor@myuax.com>
2024-02-07 19:23:29 +0100

    47

--- c.java
+++ c.java
@@ -3,3 +3,12 @@ public class TablaMultiplicar {
     public static void main(String[] args) {
         int[][] tablas = generarTablas(10);
 
+        for (int i = 0; i < tablas.length; i++) {
+            System.out.println("Tabla de multiplicar del número " + (i + 1));
+            for (int j = 0; j < tablas[i].length; j++) {
+                       System.out.printf("%d x %d = %d%n", i + 1, j + 1, tablas[i][j]);
+                     }
+               System.out.println();
+          }
+   }
+
