a411ecd79ae8b1196cc0845a99ab4b008c772728
adrianarmm <areyemor@myuax.com>
2024-02-07 19:38:09 +0100

    74

---
+++ f.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class Mediana {
+    public static void main(String[] args) {
+        int[] numeros = new int[100];
+        int cantidad = recibirNumeros(numeros);
