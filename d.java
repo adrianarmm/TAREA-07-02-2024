7d0c8631f5b05cb134aff7375fbe74e13ce7a189
adrianarmm <areyemor@myuax.com>
2024-02-07 19:26:49 +0100

    53

--- d.java
+++ d.java
@@ -2,4 +2,4 @@ import java.util.Arrays;
 
 public class TablaMultiplicar {
     public static void main(String[] args) {
-        int[][] tablas = generarTablas(10);
+        int[][] tablas = generarPrimos(10);
