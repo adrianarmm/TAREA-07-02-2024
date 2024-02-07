beacd136bb452ca98a3f06b4b291f766525e6047
adrianarmm <areyemor@myuax.com>
2024-02-07 20:03:04 +0100

    98

--- H.java
+++ H.java
@@ -6,3 +6,9 @@ public class H {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce el tamaño de la matriz cuadrada: ");
         int n = scanner.nextInt();
+
+        int[][] matriz = new int[n][n];
+        rellenarMatrizSimetrica(matriz);
+
+        imprimirMatriz(matriz);
+    }
