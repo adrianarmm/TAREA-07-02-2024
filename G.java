769058b55cbb010ec83a91328b209a5cedfd5f4a
adrianarmm <areyemor@myuax.com>
2024-02-07 19:53:04 +0100

    89

---
+++ G.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class f
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] vector1 = new int[n];
+        int[] vector2 = new int[n];
