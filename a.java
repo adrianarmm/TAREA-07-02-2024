3fcce1c26953e4ee4c585e41c2de5e8128510cc9
adrianarmm <areyemor@myuax.com>
2024-02-07 19:08:34 +0100

    20

--- a.java
+++ a.java
@@ -3,5 +3,7 @@ public class a {
         char charNumber = (char) Integer.MAX_VALUE;
         short shortNumber = Short.MAX_VALUE;
         int intNumber = Integer.MAX_VALUE;
+
+        System.out.println(" Mayor número entero representable por un char es: " + charNumber);
     }
 }
