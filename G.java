fb98c3cf4b405d1f96c3e91f85500ba7cca59f0d
adrianarmm <areyemor@myuax.com>
2024-02-07 19:53:24 +0100

    90

--- G.java
+++ G.java
@@ -6,3 +6,9 @@ public class f
         int n = scanner.nextInt();
         int[] vector1 = new int[n];
         int[] vector2 = new int[n];
+
+        System.out.println("Introduce los elementos del primer vector:");
+        leerVector(vector1);
+
+        System.out.println("Introduce los elementos del segundo vector:");
+        leerVector(vector2);
