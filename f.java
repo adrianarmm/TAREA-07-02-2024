232ffd2af2dad577ca5a7235bfee36105dbec1b1
adrianarmm <areyemor@myuax.com>
2024-02-07 19:40:40 +0100

    75

--- f.java
+++ f.java
@@ -4,3 +4,13 @@ public class Mediana {
     public static void main(String[] args) {
         int[] numeros = new int[100];
         int cantidad = recibirNumeros(numeros);
+
+
+        double media = calcularMedia(numeros, cantidad);
+        int minimo = calcularMinimo(numeros, cantidad);
+        int maximo = calcularMaximo(numeros, cantidad);
+
+        System.out.println("La media es: " + media);
+        System.out.println("El mínimo es: " + minimo);
+        System.out.println("El máximo es: " + maximo);
+    }
