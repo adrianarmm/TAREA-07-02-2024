5da32eed6741d5c021cc2fb401ed858fa99742d8
adrianarmm <areyemor@myuax.com>
2024-02-07 20:03:29 +0100

    100

--- H.java
+++ H.java
@@ -14,3 +14,15 @@ public class H {
     }
     public static void rellenarMatrizSimetrica(int[][] matriz) {
         Random random = new Random();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (i == j) {
+                    matriz[i][j] = 0;
+                } else {
+                    matriz[i][j] = random.nextInt(20) - 10;
+                    matriz[j][i] = matriz[i][j];
+                }
+            }
+        }
+    }
