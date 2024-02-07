fa8f274f3fb46232fb8f035ab786527011e1d2eb
adrianarmm <areyemor@myuax.com>
2024-02-07 19:10:51 +0100

    27

--- b.java
+++ b.java
@@ -2,5 +2,7 @@ public class b {
     public static void main(String[] args) {
         int[] multiplosDeCinco = getMultiplosDeCinco(5, 100);
         int suma = sumaDeArray(multiplosDeCinco);
+
+        System.out.println("La suma de los multiplos de 5 entre 5 y 100 es: " + suma);
     }
 }
