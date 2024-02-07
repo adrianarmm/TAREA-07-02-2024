8ad347572625aaac04983f01057460aae6e678f1
adrianarmm <areyemor@myuax.com>
2024-02-07 19:03:23 +0100

    12

--- a.java
+++ a.java
@@ -1,4 +1,1 @@
-public class a {
-    public static void main (String[] args)
-}
-System.out.println("The largest integer value representable by a char is: " + (char) Integer.MAX_VALUE);
+public class Main {
