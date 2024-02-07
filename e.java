f02ccc7893da735e123b83b6e6cc5d95c6d85431
adrianarmm <areyemor@myuax.com>
2024-02-07 19:31:51 +0100

    65

--- e.java
+++ e.java
@@ -4,3 +4,9 @@ public class FactoresPrimos {
     public static void main(String[] args) {
         int numero = 40;
         int[] factoresPrimos = descomponerEnFactoresPrimos(numero);
+
+        System.out.print("Los factores primos de " + numero + " son: ");
+        for (int i = 0; i < factoresPrimos.length; i++) {
+            System.out.print(factoresPrimos[i] + " ");
+        }
+    }
