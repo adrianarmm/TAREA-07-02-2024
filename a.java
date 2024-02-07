5a4236875d5b8cde9e983640385937b7665d6074
adrianarmm <areyemor@myuax.com>
2024-02-07 19:00:47 +0100

    10

--- a.java
+++ a.java
@@ -1,11 +1,7 @@
 public class a {
     public static void main (String[] args)
 }
-char maxChar = Character.MAX_VALUE;
 
-short maxShort = Short.MAX_VALUE;
-
-int maxInt = Integer.MAX_VALUE;
 
 System.out.println("El mayor valor para un char es: " + (int)maxChar);
 
