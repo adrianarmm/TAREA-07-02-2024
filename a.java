8cb76484fd868dc5b00401e6303eecad26629b71
adrianarmm <areyemor@myuax.com>
2024-02-07 18:57:59 +0100

    9

--- a.java
+++ a.java
@@ -9,4 +9,6 @@ int maxInt = Integer.MAX_VALUE;
 
 System.out.println("El mayor valor para un char es: " + (int)maxChar);
 
-        System.out.println("El mayor valor para un short es: " + maxShort);
+System.out.println("El mayor valor para un short es: " + maxShort);
+
+System.out.println("El mayor valor para un int es: " + maxInt);
