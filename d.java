1f48714dd60f6f081f5d998ddbc3798a9163f5e8
adrianarmm <areyemor@myuax.com>
2024-02-07 19:27:36 +0100

    54

--- d.java
+++ d.java
@@ -3,3 +3,9 @@ import java.util.Arrays;
 public class TablaMultiplicar {
     public static void main(String[] args) {
         int[][] tablas = generarPrimos(10);
+
+        System.out.println("Los 10 primeros números primos son: ");
+        for (int i = 0; i < primos.length; i++) {
+            System.out.print(primos[i] + " ");
+         }
+    }
