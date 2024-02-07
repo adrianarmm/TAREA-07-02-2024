15bf38d03dcaa7a08a97ad6634e44c2e7871664e
adrianarmm <areyemor@myuax.com>
2024-02-07 19:07:13 +0100

    16

--- a.java
+++ a.java
@@ -1,5 +1,5 @@
 public class a {
     public static void main(String[] args) {
-        System.out.println (mayor número entero representable por un char es: " + (char) Integer.MAX_VALUE);
+        char charNumber = (char) Integer.MAX_VALUE;
     }
 }
