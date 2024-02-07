df85bc045d5004828e4e461052c2e3dd2e51067c
adrianarmm <areyemor@myuax.com>
2024-02-07 19:07:53 +0100

    19

--- a.java
+++ a.java
@@ -2,5 +2,6 @@ public class a {
     public static void main(String[] args) {
         char charNumber = (char) Integer.MAX_VALUE;
         short shortNumber = Short.MAX_VALUE;
+        int intNumber = Integer.MAX_VALUE;
     }
 }
